94ade4fc081558761f7ed45a3ea89f74af44dcdb
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-07-20 21:04:13 +0200

    Swift UI Rewrite +

---
+++ Anonycord/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  Anonycord
+//
+//  Created by Constantin Clerc on 20/07/2023.
+//
+
+import Foundation
---
+++ Anonycord/VideoRecordingView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoRecordingView.swift
+//  Anonycord
+//
+//  Created by Constantin Clerc on 20/07/2023.
+//
+
+import SwiftUI
+
+struct VideoRecordingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoRecordingView()
+}
